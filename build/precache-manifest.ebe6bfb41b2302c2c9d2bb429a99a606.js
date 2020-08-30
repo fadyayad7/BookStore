7f596ebac90305acb9ec2d0ede06e6dae529e474
fadyayad7 <fady.ayad7@gmail.com>
2020-08-30 18:59:04 +0200

    adding node modules

---
+++ build/precache-manifest.ebe6bfb41b2302c2c9d2bb429a99a606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dacf4d0045c165d492297df5d8fecf5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5c5bd1ef02de26bab6b",
+    "url": "./static/css/2.1ffcf84f.chunk.css"
+  },
+  {
+    "revision": "99fc62b75a6b5b8abf71",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5c5bd1ef02de26bab6b",
+    "url": "./static/js/2.73f1a081.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.73f1a081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fc62b75a6b5b8abf71",
+    "url": "./static/js/main.fb53f3d0.chunk.js"
+  },
+  {
+    "revision": "628a9c5206835bc0291f",
+    "url": "./static/js/runtime-main.1f641f01.js"
+  }
+]);
